8c242ddb298c6e9c679feb82655b25643bfa74c8
Ayoub Khial <ayouub.khial@gmail.com>
2018-12-01 00:02:56 +0000

    Create a workspace and initial application
    - Generate Spring Boot application using Initializr
    - Generate Angular 7 application

---
+++ shops-service/src/main/java/com/unitedremote/codingchallenge/shopsservice/ShopsServiceApplication.java
@@ -0,0 +1,12 @@
+package com.unitedremote.codingchallenge.shopsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopsServiceApplication.class, args);
+	}
+}
---
+++ shops-service/src/test/java/com/unitedremote/codingchallenge/shopsservice/ShopsServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.unitedremote.codingchallenge.shopsservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShopsServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
