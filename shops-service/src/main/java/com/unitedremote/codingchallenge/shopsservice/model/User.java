78665254b9f58f076fb408f976fe2c9a69788fe4
Ayoub Khial <ayouub.khial@gmail.com>
2018-12-01 02:39:22 +0000

    Create the user model and repository

---
+++ shops-service/src/main/java/com/unitedremote/codingchallenge/shopsservice/model/User.java
@@ -0,0 +1,51 @@
+package com.unitedremote.codingchallenge.shopsservice.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+/**
+ * This class will hold the basic structure of a document in the <b>user</b> collection
+ * @author Ayoub Khial
+ * @version 1.0
+ */
+@Document(collection = "user")
+public class User {
+
+    @Id
+    private String id;
+    @Indexed(name = "user_email", unique = true)
+    private String email;
+    private String password;
+
+    public User() { }
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ shops-service/src/main/java/com/unitedremote/codingchallenge/shopsservice/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.unitedremote.codingchallenge.shopsservice.repository;
+
+import com.unitedremote.codingchallenge.shopsservice.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ * This interface is the connector between {@link User} model and MongoDB's user collection.
+ * <br>
+ * This interface will extend the <code>MongoRepository</code> class, which already contains generic methods like save
+ * (for creating/updating documents) and delete (for removing documents).
+ * @author Ayoub Khial
+ * @see MongoRepository
+ * @see User
+ */
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
